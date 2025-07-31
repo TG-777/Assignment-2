2daba5e404ed254841fda5fbd08368cd2523c2b5
Nwankwo ThankGod <nwankwothankgod911@gmail.com>
2025-07-31 22:12:56 +0100

    commit changes to leap year function

--- leap_year.dart
+++ leap_year.dart
@@ -1,11 +1,11 @@
 // Function to check if a year is a leap year
 bool isLeapYear(int year) {
-  if (year % 4 != 0) {
+  if (year % 4 == 0 && year % 100 != 0) {
+    return true;
+  } else if (year % 100 == 0 && year % 400 == 0) {
+    return true;
+  } else if (year % 100 == 0 && year % 400 != 0) {
     return false;
-  } else if (year % 100 != 0) {
-    return true;
-  } else if (year % 400 == 0) {
-    return true;
   } else {
     return false;
   }
